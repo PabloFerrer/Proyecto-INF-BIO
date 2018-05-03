6b2e7312e84bd531f5e935eb574b2235c5c93638
luisalfonso98 <luisalfonso98@gmail.com>
2018-05-03 19:48:07 +0200

    sql jar y conexion

---
+++ src/Model/Conexion.java
@@ -0,0 +1,69 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class Conexion {
+
+	String BBDDName;
+	Connection c = null;
+	Statement stmt = null;
+
+	public Conexion(String path) {
+		BBDDName = path;
+	}
+
+	public boolean sentenciaSQL(String sql) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:"+BBDDName);
+			stmt = c.createStatement();
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			return false;
+		}
+		return true;
+	}
+
+	
+	public void consulta() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			
+			c = DriverManager.getConnection("jdbc:sqlite:"+BBDDName);
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+		
+			ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+
+			while ( rs.next() ) {
+				int id = rs.getInt("id");
+				String  name = rs.getString("name");
+				int age  = rs.getInt("age");
+				String  address = rs.getString("address");
+				float salary = rs.getFloat("salary");
+
+				System.out.print( "ID = " + id );
+				System.out.print( ", NAME = " + name );
+				System.out.print( ", AGE = " + age );
+				System.out.print( ", ADDRESS = " + address );
+				System.out.println( ", SALARY = " + salary );
+			}
+
+			rs.close();
+			stmt.close();
+			c.close();
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		}
+		System.out.println("Consulta terminada");
+	}
+
+}
